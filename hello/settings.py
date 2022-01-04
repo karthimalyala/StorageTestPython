9154878faa6027fe2a9671ffc7a4a2552f2e1994
Karthi Teja Malyala <kmalyala@microsoft.com>
2022-01-04 15:28:41 +0530

    initial commit

--- hello/settings.py
+++ hello/settings.py
@@ -117,12 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'home/site/wwwroot/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/home/site/wwwroot/',
-]
+
 #PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = "/home/site/wwwroot/"
+STATIC_ROOT = "/home/site/wwwroot/static"
 
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 # Default primary key field type
