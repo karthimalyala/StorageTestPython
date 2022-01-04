5e37757b4f9745a20293bfff8c64caaab3fee32a
Karthi Teja Malyala <kmalyala@microsoft.com>
2022-01-04 15:25:27 +0530

    initial commit

--- testsas/urls.py
+++ testsas/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
  
     
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
